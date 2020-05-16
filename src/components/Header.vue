7371ace64e9e30fa05b9bd0dba91568a532ae46f
ikhidebright <ikhidebright@gmail.com>
2020-05-16 15:36:39 +0100

    res

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,86 +1,133 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-  <div class="container">
-  
-  <div class="containe d-lg-none" @click="change(this)">
-  <div class="bar1"></div>
-  <div class="bar2"></div>
-  <div class="bar3"></div>
-</div>
+  <div class="head">
+    <div>
+      <b-navbar toggleable="lg" variant="faded" fixed="top" id="header" type="light
+      ">
+        <b-navbar-brand style="font-size: 30px" href="/">CHERRY ONLINE SHOP</b-navbar-brand>
 
-  <a class="navbar-brand" href="http://localhost:8080">Shopman</a>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-  <b-button variant="p" to="/cart">
-   <i class="fas fa-cart-plus"></i>
-  </b-button><b-badge variant="l" class="cartcount"> {{ cartcount }}</b-badge>
- <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button> -->
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-    </ul>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav></b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-5">
+            <b-nav-form>
+              <input
+                size="sm"
+                id="in"
+                class="mr-sm-2"
+                placeholder=" Search products, brands, categories"
+              />
+
+              <b-button size="sm" class="my-2 my-sm-0" type="submit" variant id="search">SEARCH</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown right class="head1">
+              <template v-slot:button-content>
+                <strong>Help?</strong>
+              </template>
+              <b-dropdown-item href="#">Help Center?</b-dropdown-item>
+              <b-dropdown-item href="#">How to shop on Cherry</b-dropdown-item>
+              <b-dropdown-item href="#">How to pay on Cherry</b-dropdown-item>
+              <b-dropdown-item href="#">How to use a Voucher</b-dropdown-item>
+              <b-dropdown-item href="#">Cherry Express</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-navbar-nav class="head2">
+              <b-nav-item href="#">
+                <strong>Cart</strong>
+                <span id="cart">
+                  <i class="fas fa-cart-plus"></i>
+                  {{cartCount }}
+                </span>
+              </b-nav-item>
+            </b-navbar-nav>
+
+            <!-- <span id="cart">
+              <i class="fas fa-cart-plus"></i>
+              {{cartCount }} Cart
+            </span>-->
+
+            <b-nav-item-dropdown right class="head3" dark>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <strong>Login</strong>
+              </template>
+              <b-dropdown-item href="/login">Log In</b-dropdown-item>
+              <b-dropdown-item>
+                <router-link to="/signup" style="color: black;">Sign Up</router-link>
+              </b-dropdown-item>
+              <b-dropdown-item href="#">Cart</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
-</div>
-</nav>
 </template>
 
 <script>
-
 export default {
-  name: 'Header',
-  data () {
-    return {
-    }
-  },
+  name: "Header",
   computed: {
-    cartcount () {
-      return this.$store.getters.getCartQty
+    cartCount() {
+      return this.$store.state.cart.length;
     }
   }
-}
+};
 </script>
 
-
-<style scoped>
-.cartcount {
-  margin-bottom: 1rem;
-  margin-left: -4.5rem
-}
-
-@media only screen and (min-width: 600px) {
-  .cartcount {
-  margin-bottom: 1rem;
-  margin-left: -0.8rem
-}
-}
 <style>
-.containe {
-  display: inline-block;
-  cursor: pointer;
-  margin-right: 5rem
+#in {
+  width: 500px;
+  height: 40px;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 16px;
+  background-color: white;
+  outline: none;
+  border: 1px #c7c7cd solid;
+  
+}
+.head {
+  margin-bottom: 40px;
+}
+#search {
+  background-color: #f68b1e;
+  height: 45px;
+  border-radius: 5px;
+  color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  font-size: 14px;
+  padding: 12px 16px;
+  font-weight: 500;
+  outline-color: none;
+  outline: none;
+  border: none;
 }
 
-.bar1, .bar2, .bar3 {
-  width: 35px;
-  height: 5px;
-  background-color: #333;
-  margin: 6px 0;
-  transition: 0.4s;
+#header {
+  background-color: white !important;
+}
+.head1 {
+  color: #282828 !important;
+  font-size: 16px !important;
+  padding: 8px;
+}
+.head2 {
+  color: black !important;
+  font-size: 16px !important;
+  padding: 8px;
 }
 
-.navbar-brand {
-margin-left: 1rem
+.head3 {
+  color: #282828 !important;
+  font-size: 16px !important;
+  padding: 8px;
 }
-
-.change .bar1 {
-  -webkit-transform: rotate(-45deg) translate(-9px, 6px);
-  transform: rotate(-45deg) translate(-9px, 6px);
-}
-
-.change .bar2 {opacity: 0;}
-
-.change .bar3 {
-  -webkit-transform: rotate(45deg) translate(-8px, -8px);
-  transform: rotate(45deg) translate(-8px, -8px);
+#focus:focus {
+  outline: none !important;
 }
 </style>

6daa935bd9b744e8aa253e28a3286798bd0b85f3
ikhidebright <ikhidebright@gmail.com>
2020-05-23 12:05:33 +0100

    added more categories

--- src/components/Product/ProductCard.vue
+++ src/components/Product/ProductCard.vue
@@ -1,16 +1,16 @@
 <template>
 <div to='/'>
 <div 
-class='product'
+class='product mb-4'
 @click='open(product)'>
 <img 
 :src="`${ product.thumb }`" 
 class='pic' 
 alt='' />
 <div class='details'>
-<p class='name' v-if="product.name.length >= 19">{{ product.name.split('').slice(0, -Math.round(product.name.length/250)).join('') + '...' }} <br> 
-<b> ₦ {{ product.price.toLocaleString() }}</b> </p>
-<p class='discount mt-n4 ml-2 d-none d-lg-block d-md-block d-xl-block d-sm-block'>₦ {{ discount.toLocaleString() }}</p>
+<p class='name mb-3 mt-n2'>{{ product.name }}</p>
+<p class='priceee price mt-n2 ml-2 d-none d-lg-block d-md-block d-xl-block d-sm-block'>₦ {{ product.price.toLocaleString() }}</p>
+<p class='discount mt-n3 ml-2 d-none d-lg-block d-md-block d-xl-block d-sm-block'>₦ {{ discount.toLocaleString() }}</p>
 </div>
 </div>
 </div>
@@ -42,9 +42,9 @@ export default {
 .pic {
   width: 104px;
   height: auto;
+  margin: 0 auto;
   border-style: none;
   box-sizing: border-box;
-  text-align: center;
   -webkit-tap-highlight-color: transparent;
 }
 
@@ -56,7 +56,6 @@ p {
   margin: 0;
   font-size: .75rem;
   text-overflow: ellipsis;
-  padding-left: 16px;
   font-family: -apple-system,BlinkMacSystemFont,Roboto,"Helvetica Neue",Arial,sans-serif;
   font-weight: 400
 }
@@ -79,12 +78,49 @@ p {
   padding: 5px
 }
 
-.name {
-  text-align: center;
+ .name {
+text-align: left;
+color: #282828;
+overflow: hidden;
+text-overflow: ellipsis;
+display: -webkit-box;
+-webkit-line-clamp: 1;
+-webkit-box-orient: vertical;
+max-height: 2.824em;
+max-width: 9.824em;
+cursor: pointer;
+font-size: 13px;
+-webkit-tap-highlight-color: transparent;
+direction: ltr;
+ /* These are technically the same, but use both */
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+
+  -ms-word-break: break-all;
+
+  /* Instead use this non-standard one: */
+  word-break: break-word;
+
+  /* Adds a hyphen where the word breaks, if supported (No Blink) */
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  -webkit-hyphens: auto;
+  hyphens: auto;
 }
 
 
 @media only screen and (min-width: 600px) {
+
+  .priceee {
+    font-size: .875rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    line-height: 1;
+    direction: ltr;
+    -webkit-font-smoothing: antialiased;
+    font-weight: 700;
+    font-family: Roboto,-apple-system,BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Arial,sans-serif;
+  }
 
   .discount {
     font-size: .75rem;
@@ -96,8 +132,34 @@ p {
   .name {
   text-align: left;
   font-size: 14px;
-  padding: 8px 8px 4px;
-  color: #282828
+  padding-left: 8px;
+  color: #282828;
+   color: #000;
+overflow: hidden;
+text-overflow: ellipsis;
+display: -webkit-box;
+-webkit-line-clamp: 2;
+-webkit-box-orient: vertical;
+max-height: 2.824em;
+cursor: pointer;
+font-size: 13px;
+-webkit-tap-highlight-color: transparent;
+direction: ltr;
+ /* These are technically the same, but use both */
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+
+  -ms-word-break: break-all;
+  /* This is the dangerous one in WebKit, as it breaks things wherever */
+  word-break: break-all;
+  /* Instead use this non-standard one: */
+  word-break: break-word;
+
+  /* Adds a hyphen where the word breaks, if supported (No Blink) */
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  -webkit-hyphens: auto;
+  hyphens: auto;
 }
 
 .product {
